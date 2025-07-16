0de8400e6ac773441a7570911cc82a79cd3ec236
xhclintonxd <whitesfredd@gmail.com>
2025-07-16 20:55:54 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require('baileys-pro');
+} = require('baileys');
 
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
